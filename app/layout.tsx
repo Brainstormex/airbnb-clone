5aafff19bcd97c2c2135bba5e89b82e1ad6117a4
uzxr <uzersayed9@gmail.com>
2023-04-11 21:10:55 +0530

    Navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,28 @@
-import {Nunito} from 'next/font/google'
+import { Nunito } from "next/font/google";
 
-import './globals.css'
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
 
 export const metadata = {
-  title: 'Airnbnb',
-  description: 'airbnb clone',
-}
+  title: "Airnbnb",
+  description: "airbnb clone",
+};
 
-const font = Nunito ({
-  subsets:["latin"],
-})
-
+const font = Nunito({
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar/>
+        {children}
+        </body>
     </html>
-  )
+  );
 }
